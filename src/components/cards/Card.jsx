37a5f82137e44f02ee735ed55751df4f66588caa
Animatedguess <105106737+Animatedguess@users.noreply.github.com>
2025-02-17 23:06:05 +0530

    refactor: replace Hero component with Card component in App.jsx

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Card = () => {
   return (
     <div className="bg-amber-100 mt-12 w-96 rounded-md shadow-lg overflow-hidden">
-  <img src="./image/forms.webp" alt="image" className="w-full h-48 object-cover" />
-  <div className="flex flex-col p-4">
-    <a href="http://" className="text-lg font-semibold text-black hover:text-amber-800 transition">
-      Name of component
-    </a>
-    <p className="text-black text-sm">No. of components</p>
-    <div className="flex items-end justify-end bg-amber-200 w-fit  rounded p-1 ">
-      <a href="http:// " className="text-sm text-amber-600 hover:transition-transform duration-300 hover:scale-110">
-        Type of UI
-      </a>
+      <img
+        src="./image/forms.webp"
+        alt="image"
+        className="w-full h-48 object-cover"
+      />
+      <div className="flex flex-col p-4">
+        <div className="flex justify-between items-center">
+          <a
+            href="http://"
+            className="text-lg font-semibold text-black hover:text-amber-800 transition"
+          >
+            Name of component
+          </a>
+          <div className="flex items-end justify-end bg-amber-200 w-fit  rounded p-1 ">
+            <a
+              href="http:// "
+              className="text-sm text-amber-600 hover:transition-transform duration-300 hover:scale-110"
+            >
+              Type of UI
+            </a>
+          </div>
+        </div>
+        <p className="text-black text-sm">No. of components</p>
+      </div>
     </div>
-  </div>
-</div>
+  );
+};
 
-  )
-}
-
-export default Card
+export default Card;
