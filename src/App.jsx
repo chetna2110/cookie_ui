1e566f760f31cf59cf42880452896d1640d0767c
chetna2110 <114652508+chetna2110@users.noreply.github.com>
2025-02-17 22:53:07 +0530

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./components/header/Nav";
 import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
+import Card from "./components/cards/Card";
 export default function App() {
   return (
     <div>
       <Nav/>
       <Hero/>
+      <Card/>
       <Footer/> 
      
 
---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="bg-amber-100 mt-12 w-96 rounded-md shadow-lg overflow-hidden">
+  <img src="./image/forms.webp" alt="image" className="w-full h-48 object-cover" />
+  <div className="flex flex-col p-4">
+    <a href="http://" className="text-lg font-semibold text-black hover:text-amber-800 transition">
+      Name of component
+    </a>
+    <p className="text-black text-sm">No. of components</p>
+    <div className="flex items-end justify-end bg-amber-200 w-fit  rounded p-1 ">
+      <a href="http:// " className="text-sm text-amber-600 hover:transition-transform duration-300 hover:scale-110">
+        Type of UI
+      </a>
+    </div>
+  </div>
+</div>
+
+  )
+}
+
+export default Card
