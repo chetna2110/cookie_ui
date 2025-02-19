d72fa53a63f6d608c08fe254dbc2b2578f595161
chetna2110 <114652508+chetna2110@users.noreply.github.com>
2025-02-19 20:17:25 +0530

    sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import Nav from "./components/header/Nav";
-import Footer from "./components/footer/Footer";
-import Hero from "./components/hero/Hero";
-import Grid from "./components/grid/Grid";
-import Grid2 from "./components/grid/Grid2";
+// import Nav from "./components/header/Nav";
+// import Footer from "./components/footer/Footer";
+// import Hero from "./components/hero/Hero";
+// import Grid from "./components/grid/Grid";
+// import Grid2 from "./components/grid/Grid2";
+import Signin from "./components/signin/Signin";
 export default function App() {
   return (
     <div>
-      <Nav/>
+      {/* <Nav/>
       <Hero/>
       <Grid/>
       <Grid2/>
-      <Footer/> 
+      <Footer/>  */}
+      <Signin/>
+
      
 
     </div>
