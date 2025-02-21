668287ca51881c4d4ee16f2f53a60608858596bc
Animatedguess <105106737+Animatedguess@users.noreply.github.com>
2025-02-21 17:52:47 +0530

    add CodeSection component and integrate react-icons; update package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./components/header/Nav"
 import Footer from "./components/footer/Footer"
 import { Outlet } from "react-router-dom"
+import CodeSection from "./components/codeSection/CodeSection"
 export default function App() {
   return (
   
      <>
      <Nav/>
+     <CodeSection/>
      <Outlet/>
      <Footer/>
      </>
