8efa994abe7a61c119dfe4409b843d2a5b621c3c
chetna2110 <114652508+chetna2110@users.noreply.github.com>
2025-02-21 22:00:04 +0530

    updated code section 2

--- src/components/codeSection/CodePreview.jsx
+++ src/components/codeSection/CodePreview.jsx
@@ -154,7 +154,7 @@ const CodePreview = ({item}) => {
 
       {/* Show Code Section (Only Body Content) */}
       {showCode && (
-        <pre className="p-3 bg-amber-50 dark:bg-amber-500 text-sm rounded-md overflow-x-auto">
+        <pre className="p-3 bg-amber-100 text-sm rounded-md overflow-x-auto">
           <code>{bodyContent.trim()}</code>
         </pre>
       )}
